940ede344996179d172ff13c69de65203386ef54
GersonSurfer <gerson.enriquez@flowing.it>
2022-02-07 14:51:53 +0100

    Implement Prime Factors

--- __tests__/primeFactors.spec.ts
+++ __tests__/primeFactors.spec.ts
@@ -3,5 +3,15 @@ import {factorsOf} from '../primeFactors';
 describe("Prime Factors", () => {
   it("factors", () => {
     expect(factorsOf(1)).toEqual([])
+    expect(factorsOf(2)).toEqual([2]);
+    expect(factorsOf(3)).toEqual([3]);
+    expect(factorsOf(4)).toEqual([2, 2]);
+    expect(factorsOf(5)).toEqual([5]);
+    expect(factorsOf(6)).toEqual([2,3]);
+    expect(factorsOf(7)).toEqual([7]);
+    expect(factorsOf(8)).toEqual([2, 2, 2]);
+    expect(factorsOf(2 * 2 * 3 * 5 * 5 * 7 * 11 * 11 * 13))
+    .toEqual([2, 2, 3, 5, 5, 7, 11, 11, 13]);
+
   })
 })
--- primeFactors.ts
+++ primeFactors.ts
@@ -1,3 +1,10 @@
 export const factorsOf = (n: number) => {
-  return [];
+  const factors: number[] = [];
+  
+  for(let divisor = 2; n>1; divisor++)
+    for( ; n % divisor === 0; n/=divisor)
+      factors.push(divisor)
+  
+  return factors;
+
 }
